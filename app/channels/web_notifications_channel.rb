b7e2f13e5f16341cdba1f42222d2ed8b6d6b532d
Sudhakar P <sudharkar.p@coop.co.uk>
2018-12-17 11:55:02 +0000

    Trigger the message

--- app/channels/web_notifications_channel.rb
+++ app/channels/web_notifications_channel.rb
@@ -1,6 +1,7 @@
 class WebNotificationsChannel < ApplicationCable::Channel
   def subscribed
     stream_from "web_notifications_channel"
+    #ActionCable.server.broadcast 'web_notifications_channel', message: '<p>Hello World!</p>'
   end
 
   def unsubscribed
